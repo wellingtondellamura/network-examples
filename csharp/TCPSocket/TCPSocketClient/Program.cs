13f52570318a451d71721810a335413dfa69b9fe
Wellington Della Mura <della.mura@gmail.com>
2016-10-14 15:56:42 -0300

    A simple socket communication example using TCP

---
+++ csharp/TCPSocket/TCPSocketClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string host = "127.0.0.1";
+            int port = 7486;
+            string requestStr = "Hello World";
+            TcpClient client = new TcpClient();            
+            client.Connect(host, port);
+            NetworkStream stream = client.GetStream();
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(stream);
+            System.IO.StreamReader sr = new System.IO.StreamReader(stream);            
+            sw.Write(requestStr);
+            sw.Flush();
+            string responseStr = sr.ReadToEnd();            
+            Console.WriteLine(responseStr);            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csharp/TCPSocket/TCPSocketServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean foo = true;
+            TcpListener server = new TcpListener(IPAddress.Any, 7486);
+            server.Start();
+            while (foo)
+            {
+                Socket client = server.AcceptSocket();
+                byte[] clientData = new byte[1024];
+                int size = client.Receive(clientData);
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(Convert.ToChar(clientData[i]));
+                }
+                Console.WriteLine();
+                byte[] responseData = Encoding.ASCII.GetBytes("Bye");
+                client.Send(responseData);                
+                client.Close();
+            }
+            server.Stop();
+            Console.ReadKey();
+        }
+    }
+}
